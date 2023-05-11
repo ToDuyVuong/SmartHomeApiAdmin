2b5afc700e69d21d32410fba425b75b623f338f8
ToDuyVuong <comc5288@gmail.com>
2023-05-12 00:37:26 +0700

    Initial commit

---
+++ src/main/java/vn/smarthomeapiadmin/SmartHomeApiAdminApplication.java
@@ -0,0 +1,13 @@
+package vn.smarthomeapiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeApiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeApiAdminApplication.class, args);
+    }
+
+}
